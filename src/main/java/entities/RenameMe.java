a010444013bc320b5dc5a96ff9bd1d81fdf42f25
Andreas Vikke <vikkedesign@gmail.com>
2019-09-09 11:19:44 +0200

    Travis Test

--- src/main/java/entities/RenameMe.java
+++ src/main/java/entities/RenameMe.java
@@ -12,6 +12,7 @@ import javax.persistence.NamedQuery;
 @NamedQuery(name = "RenameMe.deleteAllRows", query = "DELETE from RenameMe")
 public class RenameMe implements Serializable {
 
+    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
