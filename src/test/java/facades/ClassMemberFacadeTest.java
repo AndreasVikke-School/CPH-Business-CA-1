0eda505d40e92cf9583ac42972695d86663d51fa
Andreas Vikke <vikkedesign@gmail.com>
2019-09-10 13:19:47 +0200

    Fixed Import

--- src/test/java/facades/ClassMemberFacadeTest.java
+++ src/test/java/facades/ClassMemberFacadeTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
