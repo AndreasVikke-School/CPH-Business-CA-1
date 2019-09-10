e78253e619cfecef526b7bd33d920d32380b5ab9
Krigermus <zyjinxx@hotmail.com>
2019-09-10 12:13:31 +0200

    jsChanged

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,13 +1,16 @@
-window.onload = function() {
-document.getElementById("reload").addEventListener("click", function(){
-   startFetch("/CA-1/api/classmember/all");
-    });
+window.onload = function () {
+    document.getElementById("reload").onclick = async() => {
+        var data = await startFetch("/CA-1/api/classmember/all");
+        document.getElementById("tableData").innerHTML = data.map(x => "<tr><td>" + x.name + "</td><td>" + x.studentId + "</td><td>" + x.color + "</td></tr>").join(" ");
+    };
 }
+var dataOut;
 
-function startFetch(url) {
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-      document.getElementById("tableData").innerHTML =  data.map(x => "<tr><td>" + x.name + "</td><td>" + x.studentId + "</td><td>" + x.color + "</td></tr>").join(" ");
-});
+const startFetch = async(url) => {
+    await fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                dataOut = data;
+            });
+            return dataOut;
 };
